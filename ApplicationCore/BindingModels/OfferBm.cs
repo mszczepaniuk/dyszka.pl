9562941eea3686a76cf91535f423bf2f3ce4ac09
Maciej Szczepaniuk <m.szczepaniuk@npc.pl>
2020-09-18 22:10:09 +0200

    Back-end validation

--- ApplicationCore/BindingModels/OfferBm.cs
+++ ApplicationCore/BindingModels/OfferBm.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ApplicationCore.BindingModels
 {
     public class OfferBm
     {
+        [Required]
         public string Image { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Title { get; set; }
+        [Required]
         public IList<string> Tags { get; set; }
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
+        [Required]
+        [MaxLength(160)]
         public string ShortDescription { get; set; }
+        [Required]
+        [Range(10, double.PositiveInfinity)]
         public decimal Price { get; set; }
     }
 }
